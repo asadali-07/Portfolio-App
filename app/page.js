ffc2361886d96a1551602e6799f60767bdf9b8e5
Asad Ali <mo.asadali007@gmail.com>
2025-03-01 18:51:47 +0530

    add bounce animation

--- app/page.js
+++ app/page.js
@@ -87,7 +87,7 @@ function Home() {
       </section>
       <section id="contact" className="py-20">
       <div className="container mx-auto px-6">
-        <h2 className="text-4xl font-bold mb-8 text-purple-500 text-center">Get In Touch</h2>
+        <h2 className="text-4xl font-bold mb-8 text-purple-500 text-center animate-bounce">Get In Touch</h2>
         <div className="max-w-lg mx-auto p-8 flex gap-5 justify-center align-middle rounded-lg shadow-lg w-96 text-center">
           <div className="mb-4">
             <a href="mailto:mo.asadali007@gmail.com" className="text-blue-500 hover:underline">
--- components/Navbar.js
+++ components/Navbar.js
@@ -38,8 +38,8 @@ const Navbar = () => {
       />
       <div className="container mx-auto flex justify-around items-center">
         <div className=" text-lg font-bold  flex justify-center items-center gap-3 text-purple-500">
-          <Image src="/resume.png" width={30} height={30} className="rotate-6"/>
-        <Link href="/" className="text-2xl"><i>AsadAli</i></Link>
+          <Image src="/resume.png" width={30} height={30} className="animate-bounce"/>
+        <Link href="/" className="text-2xl animate-bounce"><i>AsadAli</i></Link>
         </div>
         <div className="hidden md:flex space-x-6 items-center">
           <Link
